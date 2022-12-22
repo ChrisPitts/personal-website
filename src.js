26546bc52bcd20786eeffc29822ede06c395caf9
Chris Pitts <cp25393@gmail.com>
2022-12-21 23:04:29 -0600

    added typewriter animation

---
+++ src.js
@@ -0,0 +1,36 @@
+let options = {
+    threshold: 1.0
+};
+
+let timer = 0;
+function typeWriter(target, paragraph, index){
+    if(index < paragraph.length){
+        target.innerHTML += paragraph[index];
+        index++;
+        setTimeout(()=>{typeWriter(target, paragraph, index)}, 10);
+    }
+    else
+        target.classList.remove('animated');
+}
+
+function callback(entries, observer){
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            let paragraph = entry.target.innerHTML;
+            entry.target.innerHTML = "";
+            entry.target.classList.remove("unloaded");
+            setTimeout(()=>{typeWriter(entry.target, paragraph, 0)}, timer);
+            timer += 1000;
+            setTimeout(()=>{timer=0}, 1000);
+
+            // entry.target.classList.remove('animated')
+                
+            
+            observer.unobserve(entry.target);
+        }
+    })
+}
+
+let observer = new IntersectionObserver(callback, options);
+let targets = document.querySelectorAll('.unloaded');
+targets.forEach((target)=>observer.observe(target));
